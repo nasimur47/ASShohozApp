bdff1043a1bc941666f1d9f9bdcbd7e5218d90e3
nasimur47 <mdnasimurrahman11@gmail.com>
2022-05-11 20:54:27 +0600

    hellow

---
+++ app/src/main/java/com/nasim/myapplication_50/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nasim.myapplication_50;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+}
